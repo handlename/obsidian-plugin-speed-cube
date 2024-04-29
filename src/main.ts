29067227560945d0c10ae733ea5c27985c3ccb5d
handlename <nagata@handlena.me>
2024-04-29 17:59:34 +0900

    feat: remove added tables on unload

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,16 @@
 import { MarkdownPostProcessorContext, Plugin } from 'obsidian';
 import { TopLayerParser, TopLayerElementBuilder } from './layer';
+import { HTML_CLASS_NAME } from './constants';
 
 export default class SpeedCubePlugin extends Plugin {
 	async onload() {
 		this.registerMarkdownCodeBlockProcessor("speedcube", this.codeProcessor)
+	}
+
+	async onunload() {
+		document.querySelectorAll(`.${HTML_CLASS_NAME}`).forEach((el) => {
+			el.remove();
+		});
 	}
 
 	async codeProcessor(src: string, el: HTMLElement, ctx: MarkdownPostProcessorContext) {
